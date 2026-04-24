9a690683b93dc0e871f2b13ca233423e2f9a4a74
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-04-24 06:31:28 +0600

    expo build

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import "./global.css"
 
 export default function App() {
-  return (
 
-    <View className="flex-1 items-center justify-center bg-green-700">
-      <Text className="text-sky-500 font-bold italic">i am shariful alam</Text>
-      <Text className="text-sky-500 font-bold italic">how are you9</Text>
+  const googleLogin = () => {
+    console.log("Google Login Pressed");
+  }
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-50">
+
+      <Text className="text-sky-500 font-bold italic mb-10 text-xl">
+        i am shariful alam
+      </Text>
+
+      {/* Google Login Button */}
+      <TouchableOpacity
+        activeOpacity={0.8}
+        className="flex-row items-center bg-white px-6 py-3 rounded-full border border-gray-200 shadow-sm"
+        onPress={googleLogin}
+      >
+        <Image
+          source={{ uri: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png' }}
+          className="w-6 h-6 mr-3"
+        />
+        <Text className="text-gray-700 font-semibold text-base">
+          Continue with Google
+        </Text>
+      </TouchableOpacity>
 
     </View>
   );
